cc288313ce30c0d5aca4f3e3cecce50173b1fb0f
dannv6 <dannv6@fpt.com.vn>
2019-10-24 14:10:36 +0700

    Updating user URLConf

--- user/urls.py
+++ user/urls.py
@@ -9,4 +9,5 @@ app_name = 'user'
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
     path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
